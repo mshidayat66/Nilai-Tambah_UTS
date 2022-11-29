19a5635b524207559af3fa76d9ced19a1460d558
Muhammad Syarif Hidayat <92723671+mshidayat66@users.noreply.github.com>
2022-11-29 17:07:16 +0700

    Add File UTS

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
-package com.hitung;
-
 import com.bentuk.*;
+import com.hitung.*;
 
 import java.util.Scanner;
 
@@ -8,17 +7,19 @@ public class Main {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        int p,l;
+        System.out.print("masukan panjang: ");
+        p = sc.nextInt();
+        System.out.print("masukan lebar: ");
+        l = sc.nextInt();
 
-        int x,y;
-        x = sc.nextInt();
-        y = sc.nextInt();
-
-        Lingkaran lingsatu = new Lingkaran(x,y);
+        Lingkaran lingsatu = new Lingkaran(y);
         Persegi persegisatu = new Persegi(x,y);
 
         HitungArsir pertama = new HitungArsir(lingsatu, persegisatu);
         pertama.tampilkanHasil();
-
+        
         sc.close();
     }
 }
